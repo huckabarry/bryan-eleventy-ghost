2ab7a236470b2d94fa71fc4ffcfbbef8f4ddbf35
huckabarry <bryan.robb@gmail.com>
2026-03-03 21:51:00 -0800

    Some things

--- src/_data/site.js
+++ src/_data/site.js
@@ -22,10 +22,14 @@ module.exports = async function() {
   if (process.env.SITE_URL) {
     data.url = process.env.SITE_URL;
   }
+  const normalizedSiteUrl = /^https?:\/\//i.test(data.url || "")
+    ? data.url
+    : `https://${data.url || ""}`;
   data.title = "Afterword";
   data.description = "Hey! I’m Bryan, an urban planner and design thinker living in the Pacific Northwest with my wife and two young kids. Afterword is a blog where I post about everyday life.";
   data.logo = "https://cdn.u.pika.page/2S770Bf-Ta8Bf_SF3tDUa-2fIeZocDjl3ewqMmBvJSk/fn:IMG_8710/plain/s3://pika-production/aj4090bxube83j7rhj151mum2ssn";
-  data.domain = new URL(data.url).hostname;
+  data.url = normalizedSiteUrl;
+  data.domain = new URL(normalizedSiteUrl).hostname;
   data.atprotoDid = process.env.ATPROTO_DID || "did:plc:vt4k6d3e5rjw65cuzaf3nufq";
   data.webfinger = {
     username: "bryan",
